e0b51510c1c99d42ba96a32da7d06c165f37e9ce
sadham <02sadham@gmail.com>
2025-12-23 20:27:35 +0530

    sample commit from SIT branch

--- Sample Project for Mohan_2025/src/test/java/Sample.java
+++ Sample Project for Mohan_2025/src/test/java/Sample.java
@@ -1,7 +1,5 @@
 public class Sample {
     public static void main(String[] args){
-        System.out.println("Sample Print");
-        System.out.println("Sample Print");
-        System.out.println("Sample Print");
+        System.out.println("Sample Code from SIT branch");
     }
 }
